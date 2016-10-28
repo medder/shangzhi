7c2950b5b41aac85c1c93a40482b3ad8b78d6e7f
lv_medder <gaopan_lv@163.com>
2016-10-28 11:12:38 +0800

    coding

--- zongfuzaixian/views/zong_test.py
+++ zongfuzaixian/views/zong_test.py
@@ -1,9 +1,32 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
+import hashlib
 
-from flask import Blueprint
+from flask import Blueprint,request
 
 mod = Blueprint('zong_test', __name__, url_prefix='/wx/')
 
-@mod.route('/test/')
+@mod.route('/')
 def zong_test():
-    return 'Hello Zongfuzaixian'
+    args = request.args.to_dict()
+    if len(args) == 0:
+        return 'Hello Zongfuzaixian'
+    else:
+        signature = args.get('signature')
+        timestamp = args.get('timestamp')
+        nonce = args.get('nonce')
+        echostr = args.get('echostr')
+
+        token = "shagnzhikeji"
+
+        ls = [token, timestamp, nonce]
+        ls.sort()
+
+         sha1 = hashlib.sha1()
+         map(sha1.update, ls)
+         hashcode = sha1.hexdigest()
+
+        print "handle/GET func: hashcode, signature: ", hashcode, signature
+        if hashcode == signature:
+            return echostr
+        else:
+            return ""
