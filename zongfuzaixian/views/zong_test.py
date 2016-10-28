bdb31b2b59ad1b23c2495638138b76b0bbcdba2b
lv_medder <gaopan_lv@163.com>
2016-10-28 11:16:31 +0800

    coding

--- zongfuzaixian/views/zong_test.py
+++ zongfuzaixian/views/zong_test.py
@@ -8,6 +8,7 @@ mod = Blueprint('zong_test', __name__, url_prefix='/wx/')
 @mod.route('/')
 def zong_test():
     args = request.args.to_dict()
+    print(args)
     if len(args) == 0:
         return 'Hello Zongfuzaixian'
     else:
@@ -21,9 +22,9 @@ def zong_test():
         ls = [token, timestamp, nonce]
         ls.sort()
 
-         sha1 = hashlib.sha1()
-         map(sha1.update, ls)
-         hashcode = sha1.hexdigest()
+        sha1 = hashlib.sha1()
+        map(sha1.update, ls)
+        hashcode = sha1.hexdigest()
 
         print "handle/GET func: hashcode, signature: ", hashcode, signature
         if hashcode == signature:
